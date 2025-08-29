4869d77a6fba54e7d0f1e46c200c3ac2c6617cfa
iproazad <125834441+iproazad@users.noreply.github.com>
2025-08-30 02:10:30 +0300

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	presets: ["module:metro-react-native-babel-preset"],
-	plugins: [["module:react-native-dotenv"]],
-	env: {
-		production: {
-			plugins: ["react-native-paper/babel"],
-		},
-	},
-};
